f352c834d0ee9966abe34c694f298bc5343a2872
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2026-04-06 16:55:53 +0700

    update

---
+++ src/main/java/com/example/sof302202/Sof302202Application.java
@@ -0,0 +1,13 @@
+package com.example.sof302202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sof302202Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sof302202Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sof302202/Sof302202ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sof302202;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sof302202ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
